ab308f0b6a6464178035a6a36fbe525aeefb65a7
Eric Hunzeker <emh128.pitt.edu>
2018-04-28 12:53:37 -0400

    hmmm adding dropdown menu to support different get methods???

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.css';
-import {UserTopArtists} from "./UserTopArtists";
 import {UsernameForm} from "./UsernameForm";
-
-
-
-
 
 class App extends Component {
     render() {
         return (
             <div>
-                <UsernameForm/>
+                <UsernameForm />
             </div>
         );
   }
--- src/components/UsernameForm.js
+++ src/components/UsernameForm.js
@@ -1,40 +1,64 @@
 import React, { Component } from 'react'
-import {UserTopArtists} from "./UserTopArtists";
-import ReactDOM from 'react-dom';
+import '../stylesheets/App.css';
 
 
 export class UsernameForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: props.name
+            name: props.name,
+            topartists: [],
+            image: []
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+
     handleChange(event) {
         this.setState({name: event.target.value});
     }
 
     handleSubmit(event) {
-        ReactDOM.render(<UserTopArtists name={this.state.name} />, document.getElementById('root'));
+        this.setState({name: event.target.value});
+        this.componentDidMount();
         event.preventDefault();
     }
 
+    componentDidMount() {
+        fetch("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=" + this.state.name + "&api_key=9c8b2e5697dab4cd4e70e2c5f17a98dc&limit=10&period=7day&format=json")
+            .then(res => res.json())
+            .then(
+                (artist) => {
+                    this.setState({
+                        topartists: artist.topartists.artist
+                    });
+                },
+            )
+    }
+
     render() {
+        const { topartists } = this.state;
+
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        username:
-                        <textarea value={this.state.name} onChange={this.handleChange} />
+                        <input type="text" value={this.state.name} placeholder="last.fm username" onChange={this.handleChange}/>
                     </label>
-                    <input type="submit" value="Submit" />
                 </form>
 
-                <UserTopArtists name={this.state.name}/>
+                <div className="topresults">
+                    {topartists.map(artist => (
+                        <div>
+                            <img src={artist.image[2]['#text']}/>
+                            <p> {artist.name}{" - "}{artist.playcount}{" plays in 7 days"} </p>
+                        </div>
+                    ))}
+                </div>
+
+
             </div>
         );
     }
